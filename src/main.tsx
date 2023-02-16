74588fb4d6e0ce62ab5e8ad03f8c11c95192b539
bob <bobluoluo@hotmail.com>
2023-02-15 21:08:49 -0500

    more primitives added

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 // import './index.css'
 
-import App from './BundleDraw/App'
+//import App from './BundleDraw/App'
+import App from './Offscreen/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
